c07525226171300c131127f2d5442e97f27595ff
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-07-27 08:54:12 -0300

    Adding the cards equal to the selected number of cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
+let cards;
 
 function howManyCards() {
-    const cards = prompt(`Quantas cartas quer utilizat?\nPor favor entre um número par entre 4 e 14`);
+    cards = prompt(`Quantas cartas quer utilizat?\nPor favor entre um número par entre 4 e 14`);
 
     if (Number(cards) && Number.isInteger(Number(cards)) && (Number(cards) >= 4 || Number(cards) <= 14) && (Number(cards) % 2 === 0)) {
         const card = document.querySelector('.card');
         for (let i = 1; i <= Number(cards) - 1; i++) {
             let newCard = card.cloneNode(true);
             newCard.id = i;
+            document.querySelector('.cards').appendChild(newCard);
         }
     }
     else {
@@ -18,6 +20,7 @@ function toggleImg(element) {
     const imgs = element.querySelectorAll('.card-img');
     imgs.forEach(i => i.classList.toggle('hided'));
 }
+
 function clickCard(element) {
     toggleImg(element);
 }
